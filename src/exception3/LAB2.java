686059c92544960694fca2fb2ae6a899bfd71e15
nalinee <nalinee.nil@gmail.com>
2023-03-28 16:31:44 +0700

    Exception3 part 2

--- src/exception3/LAB2.java
+++ src/exception3/LAB2.java
@@ -12,7 +12,7 @@ public class LAB2 {
     public static void main(String[] args) {
         int[] list = new int[10];
         try{
-            System.out.println(" list[10 is " + list[10]);
+            System.out.println(" list[10] is " + list[10]);
         }
         catch(ArithmeticException e) {
             System.out.println("ArithmeticException");
